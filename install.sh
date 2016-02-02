c8b0389d54816e353fd9369b9a60285a3731d8bb
Rodrigo Siqueira <siqueira@kuniri.org>
2016-02-01 22:35:14 -0200

    Simple improvement on installation script

--- install.sh
+++ install.sh
@@ -1,17 +1,49 @@
 #!/bin/bash
 
+set -e
+
+BLUECOLOR="\033[1;34;49m%s\033[m\n"
+REDCOLOR="\033[1;31;49m%s\033[m\n"
+
+function say()
+{
+  message="$@"
+  if [ -t 1 ]; then
+    printf $BLUECOLOR "$message"
+  else
+    echo "$message"
+  fi
+}
+
+function complain()
+{
+  message="$@"
+  if [ -t 1 ]; then
+    printf $REDCOLOR "$message"
+  else
+    echo "$message"
+  fi
+}
+
+function clean_legacy()
+{
+  say "Clear old vim..."
+  trashVim=$(mktemp -d)
+  mv $HOME/.vim $trashVim
+  mv $HOME/.vimrc $trashVim
+}
+
+# First clean old vimrc
+clean_legacy
+
 cp .vimrc ~/.vimrc
 
-echo
-echo "vimrc installed into $HOME/.vimrc"
-echo
+say "vimrc installed into $HOME/.vimrc"
 
 mkdir -p ~/.vim/
 rsync -vr .vim/ ~/.vim
 
-echo
-echo "Vim plugins installed into $HOME/.vim/"
-echo
+say "Vim plugins installed into $HOME/.vim/"
 
 if [[ `uname` == 'Darwin' ]]; then
   # MacOS
@@ -29,5 +61,4 @@ if [[ -n `which fc-cache` ]]; then
   fc-cache -f $font_dir
 fi
 
-echo
-echo "Fonts installed into $font_dir"
+say "Fonts installed into $font_dir"
